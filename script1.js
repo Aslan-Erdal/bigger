28d4beee6f501cd245ee27b7599d04024e70f46b
Aslan-Erdal <aslan.erdal.dev@gmail.com>
2024-03-12 10:51:02 +0100

    feat/biggerNumber : bigger number js script file added into index.html

---
+++ script1.js
@@ -0,0 +1,10 @@
+console.log('Hello World!');
+
+console.log('---------- BIG NUMBER ---------');
+let bigNumber = [1, 2, 4 , 10 , 5 , 3 , 11, 15, 6, 7 ];
+
+let bigger = Math.max(...bigNumber);
+
+console.log({ BIGNUMBER : bigger });
+
+console.log('---------- END OF BIG NUMBER ---------');
